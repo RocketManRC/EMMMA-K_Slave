4d438fc9b25bf9eeabcb02257da9124e4e10ed4d
RocketManRC <rocketmanrc@gmail.com>
2022-02-18 13:16:55 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,26 @@
   This is the code for the EMMMA-K "Slave" processor. When the Master asks
   it sends the state of the remaining 6 note keys and another touch pin
   on the back of the case used to enable pitch bends.
+
+  Updated on December 2, 2020 for EMMMA-K-v2 to add pin 1 - now 8 pins from 7. Also added
+  a second byte to send the pushbuttons on pins 2 and 3 (pin 4 used as the ground reference).
 */
 
 #include <touchablePin.h>
 
-touchablePin pins[7];
-uint8_t pinNumbers[] = {18,15,19,16,22,17,0};
+touchablePin pins[8];
+uint8_t pinNumbers[] = {18,15,19,16,22,17,0,1};
 
 void setup() 
 { 
   Serial2.begin(115200);
 
-  for(int i = 0; i < 7; i++)
+  for(int i = 0; i < 8; i++)
       pins[i].setPin(pinNumbers[i]);
+
+  //pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
 }
 
 void loop() 
@@ -23,7 +30,7 @@ void loop()
   {
     uint8_t p = 0;
 
-    for(int i = 0; i < 7; i++)
+    for(int i = 0; i < 8; i++)
     {
       if(pins[i].isTouched())
       {
@@ -32,5 +39,15 @@ void loop()
     }
 
     Serial2.write(p);
+
+    uint8_t p2 = 0;
+
+    if(!digitalRead(3))
+      p2 |= 2;
+
+    if(!digitalRead(4))
+      p2 |= 1;
+
+    Serial2.write(p2);
   }
 }
